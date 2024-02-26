1da1c1bab15b17721b1f957ba391b643bfb79155
RAJAKUMAR S <118249464+rajakumar8153@users.noreply.github.com>
2024-02-26 10:09:50 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,7 @@ const Contact = () => {
     const email = document.querySelector('input[name="email"]').value;
     const message = document.querySelector('textarea[name="message"]').value;
 
-    const mailtoLink = `mailto:727822tuec116@skct.edu.in?subject=Collaboration Request&body=Name: ${name}%0D%0AEmail: ${email}%0D%0AMessage: ${message}`;
+    const mailtoLink = `mailto:zuck@fb.com?subject=Collaboration Request&body=Name: ${name}%0D%0AEmail: ${email}%0D%0AMessage: ${message}`;
     window.location.href = mailtoLink;
   };
 
@@ -15,7 +15,7 @@ const Contact = () => {
       <form className='flex flex-col max-w-[600px] w-full'>
         <div className='pb-8'>
           <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
-          <p className='text-gray-300 py-4'>Submit the form below or shoot me an email - 727822tuec108@skct.edu.in</p>
+          <p className='text-gray-300 py-4'>Submit the form below or shoot me an email - zuck@fb.com</p>
         </div>
         <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />
         <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' />
